e3995f54eadc80b366827aa516f0e0825baf8469
ahmed <rifkiahmed.91@gmail.com>
2023-06-21 19:16:29 +0000

    Inserts one element into the stack

---
+++ push.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+#include <stdio.h>
+/**
+ * push - push an element to the stack
+ * @stack: pointer to the stack
+ * @line_number: integer line number of opcode
+ *
+ * Return: Nothing
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ele;
+
+	ele = malloc(sizeof(stack_t));
+
+	if (ele == NULL)
+	{
+		write(2, "Error: malloc failed\n", 21);
+		exit(EXIT_FAILURE);
+	}
+	if (var.data == NULL || is_digit(var.data) == -1)
+	{
+		printf("L<%u>: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	ele->n = atoi(var.data);
+	ele->prev = NULL;
+	if (*stack == NULL)
+	{
+		ele->next = NULL;
+		*stack = ele;
+	}
+	else
+	{
+		ele->next = *stack;
+		*stack = ele;
+	}
+}
