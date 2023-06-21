d6df422a34da8f95035de45cfcd4b9945e918fc9
ahmed <rifkiahmed.91@gmail.com>
2023-06-21 19:14:45 +0000

    add header file

--- monty.h
+++ monty.h
@@ -1,6 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
-
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -16,8 +21,6 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -31,5 +34,24 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
+/**
+ * struct var_s - arguments
+ * @ele: element to add or remove from stack
+ * @line_no: opcode number of line
+ * @data: integer
+ *
+ * Description: global variable
+ */
+typedef struct var_s
+{
+	stack_t *ele;
+	unsigned int line_no;
+	char *data;
+} var_t;
+extern var_t var;
+int is_digit(char *s);
+void (*get_opcode(char *s))();
+void invalid_instr(char *msg, ...);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 #endif
