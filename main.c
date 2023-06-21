aea6dff14fd20aa62902267dde97fc1ba79fdabb
ahmed <rifkiahmed.91@gmail.com>
2023-06-21 21:29:29 +0000

    Fix checks

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		write(2, "USAGE: monty file\n", 18);
+		invalid_instr("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
