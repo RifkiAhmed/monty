2accb266a5b7906524cdc5ac1c5d737f3a436a83
ahmed <rifkiahmed.91@gmail.com>
2023-06-21 19:19:09 +0000

    Checks if argument is digital number

---
+++ is_digit.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * is_digit - checks if argument given is of type integer
+ * @s: pointer to string
+ *
+ * Return: 0 if @s is integer or -1 if not
+ */
+int is_digit(char *s)
+{
+	int i;
+	if (s == NULL)
+		return (-1);
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if(*(s + i) >= '0' && *(s + i) <= '9')
+			i++;
+		else
+			return (-1);
+	}
+	return (0);
+}
