7a0bff6b98a13f08a62ff1438dc645a46e25c397
ahmed <rifkiahmed.91@gmail.com>
2023-06-22 00:54:27 +0000

    Fix checks

---
+++ tokens_number.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * count_tokens - counts number of words in @str
+ * @str: pointer to string
+ *
+ * Return: number of tokens
+ */
+unsigned int tokens_number(char *str)
+{
+	unsigned int count = 0;
+
+	while (*str)
+	{
+		while (isspace(*str))
+			++str;
+		if (!*str)
+			break;
+		do {
+			++str;
+		} while (!isspace(*str) && *str);
+
+		++count;
+	}
+	return (count);
+}
+
